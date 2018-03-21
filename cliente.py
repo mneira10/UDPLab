7663356b40e4ff266299e1d783f5e280aab709a5
Mauricio Neira <m.neira10@uniandes.edu.co>
2018-03-21 14:30:03 -0500

    pasado a python3

--- cliente.py
+++ cliente.py
@@ -1,15 +1,30 @@
 import socket
 
-IP = raw_input("IP: ")
-puerto = raw_input("Puerto: ")
-numObj = raw_input("Numero de objetos a mandar: ")
-print "-------------------------------"
-print "IP destino: ", IP
-print "Puerto destino: ", puerto
-print "Numero de mensajes: ", numObj
-print "-------------------------------"
-print "Conectando "
+IP = input("IP: ")
+puerto = int(input("Puerto: "))
+numObj = input("Numero de objetos a mandar: ")
+print ("-------------------------------")
+print ("IP destino: ", IP)
+print ("Puerto destino: ", puerto)
+print ("Numero de mensajes: ", numObj)
+print ("-------------------------------")
+print ("Conectando por TCP...")
 
-sock = socket.socket(socket.AF_INET, # Internet
-                      socket.SOCK_DGRAM) # UDP
-sock.sendto(MESSAGE, (IP, puerto))
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((IP, puerto))
+s.send(numObj.encode())
+data = s.recv(1024)
+print ("Data recieved: ", data.decode())
+if(data == "OK"):
+    s.close()
+
+
+
+
+
+
+print( "received data:", data)
+
+# sock = socket.socket(socket.AF_INET, # Internet
+#                       socket.SOCK_DGRAM) # UDP
+# sock.sendto(MESSAGE, (IP, puerto))
--- servidor.py
+++ servidor.py
@@ -1,11 +1,26 @@
 import socket
 
-UDP_IP = "127.0.0.1"
-UDP_PORT = 5005
+IP = "127.0.0.1"
+puerto = 5007
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.bind((UDP_IP, UDP_PORT))
 
-while True:
-    data, addr = sock.recvfrom(1024) #buffersize = 1024
-    print data
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((IP, puerto))
+s.listen(1)
+
+conn, addr = s.accept()
+print( 'Conexion TCP activa en direccion:', addr)
+while 1:
+    data = conn.recv(1024)
+    if not data: break
+    print ("received data:", data.decode())
+    conn.send("OK".encode())  # echo
+conn.close()
+print( 'Conexion TCP cerrada en direccion:', addr)
+
+# sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+# sock.bind((UDP_IP, UDP_PORT))
+
+# while True:
+#     data, addr = sock.recvfrom(1024) #buffersize = 1024
+#     print( data)
